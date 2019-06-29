7e8c6b8bd5c7df9afb5adbc8ba64ae70824c47f2
ajkim19 <ajkim19@gmail.com>
2019-06-28 17:44:08 -0700

    Updated files

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,80 @@
-d3.json("/data").then((data) => {
-  
-  console.log(data["fossil_rank"])
-  
+d3.json("/data").then((energy_data) => {
+
+  function drawRegionsMap() {
+    let populationData = [['Country', 'Population']];
+    
+    for (let i=0; i<energy_data.population.length; i++) {
+      populationData.push([energy_data.name[i], energy_data.population[i]]);
+    }
+    console.log(populationData)
+    let data = google.visualization.arrayToDataTable(populationData);
+
+    let options = {
+      colorAxis: {colors: ['white', 'green']},
+      backgroundColor: '#81d4fa',
+      defaultColor: '#f5f5f5',
+    };
+
+    let chart = new google.visualization.GeoChart(document.getElementById('map'));
+
+    chart.draw(data, options);
+  }
+
+  google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+
+  google.charts.setOnLoadCallback(drawRegionsMap);
+
+  // Bar Graph
+  let population_trace = {
+    x: energy_data.name.slice(0,10),
+    y: energy_data.big_spenders.slice(0,10),
+    type: "bar"
+  };
+
+  let population_layout = {
+    title: "Top 10 Population",
+    xaxis: { title: "Countries" },
+    yaxis: { title: "Big Spenders" }
+  };
+
+  Plotly.newPlot("chart1", [population_trace], population_layout);
+
+
+  console.log(energy_data)
+  // Scatter Plot
+  var population_econsumption = {
+    x: energy_data.gdppc,
+    y: energy_data.econsumption/energy_data.population,
+    mode: "markers",
+    type: "scatter",
+    name: "energy consumption",
+    marker: {
+      color: "#2077b4",
+      symbol: "hexagram"
+    }
+  };
+
+  let PE_layout = {
+    title: "Energy Consumption vs Population",
+    xaxis: { title: "Population" },
+    yaxis: { title: "Energy Consumption" }
+  };
+
+  Plotly.newPlot("chart2", [population_econsumption], PE_layout);
+
+
+
+
+
+
+
+
+
 });
+
+
